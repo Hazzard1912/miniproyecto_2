6842c6cd55ffb96a205c8edaf9c3fa52fa17930e
Juan CV <jacklazerjscv@gmail.com>
2022-11-14 17:42:46 -0500

    Añadi comentarios

--- Miniproyecto_2/src/actores/Figura.java
+++ Miniproyecto_2/src/actores/Figura.java
@@ -9,10 +9,14 @@ package actores;
  * @authors: Jhonnier Hernandez Juan Cifuentes Alejandra Carvajal
  */
 public class Figura {
-
+    
+    // Es la lista de las rutas de las imagenes con figuras, es static porque solo se necesita una para todas las figuras.
     private static String[] listaDeRutasDeImagenes = {"/imagenes/figuras/circulo.png", "/imagenes/figuras/cuadrado.png", "/imagenes/figuras/triangulo.png"};
+    // Este atributo indica la ruta de la imagen que tiene la figura (circulo, triangulo, cuadrado, etc), este puede cambiar con el metodo setRutaDeImagen()
     private String rutaDeImagen;
+    // Este atributo indica si ya se ha usado el metodo setRutaDeImagen() en el objeto (instancia) creado
     private boolean setRutaDeImagenSinUsar;
+    // Este atributo indica si la figura ya se a observado (o revelado despues de que el contador finalizo)
     private boolean figuraObservada;
 
     private String forma;
@@ -39,6 +43,7 @@ public class Figura {
         this.color = color;
     }
 
+    // Cambia la ruta de la imagen. Este metodo se ejecuta cuando se inicia una ronda
     public void setRutaDeImagen() {
         if (setRutaDeImagenSinUsar == true) {
             setRutaDeImagenSinUsar = false;
@@ -49,10 +54,12 @@ public class Figura {
         figuraObservada = false;
     }
 
+    // Dice la ruta de la imagen. Este metodo se ejecuta cuando se da click a una ficha
     public String getRutaDeImagen() {
         return rutaDeImagen;
     }
 
+    // Hace que el label relacionado con lafigura muestre en pantalla la imagen (circulo, cuadrado, etc). Este metodo se ejecuta cuando se da click a una ficha despues de finalizado el contador
     public void setRutaDeImagenTo(javax.swing.JLabel labelX) {
         if (setRutaDeImagenSinUsar == true) {
             setRutaDeImagen();
@@ -60,14 +67,17 @@ public class Figura {
         labelX.setIcon(new javax.swing.ImageIcon(getClass().getResource(rutaDeImagen)));
     }
 
+    // Cambia el atributo figuraObservada a falso. Este metodo se ejecuta cuando se da click a una ficha despues de finalizado el contador
     public void observarFigura() {
         figuraObservada = true;
     }
 
+    // Dice si es verdadero o falso que la figura ya fue observada.  Este metodo se ejecuta cuando se da click a una ficha despues de finalizado el contador
     public boolean getFiguraObservada() {
         return figuraObservada;
     }
 
+    // Quita cualquier imagen que tenga a la figura. Este metodo se ejecuta cuando se establece que figuras se van a mostrar y cuales no.
     public void anularRutaDeImagen(javax.swing.JLabel labelX) {
         if (setRutaDeImagenSinUsar == true) {
             setRutaDeImagenSinUsar = false;
